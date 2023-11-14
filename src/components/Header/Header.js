7367db15053b9645a2f122312a09cc11ebe6048b
Towkir Ahmed <towkir17@gmail.com>
2023-11-15 01:12:14 +0600

    added theme toggler component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="header">
       <h1>devfinder</h1>
-      <ThemeToggler theme={'dark'} />
+      <ThemeToggler />
     </header>
   )
 }
--- src/components/ThemeToggler/ThemeToggler.js
+++ src/components/ThemeToggler/ThemeToggler.js
@@ -1,9 +1,30 @@
-function ThemeToggler({ theme }) {
+import IconMoon from 'components/InteractiveIcons/IconMoon/IconMoon';
+import IconSun from 'components/InteractiveIcons/IconSun/IconSun';
+import './ThemeToggler.css';
+import { useState, useEffect } from 'react';
+function ThemeToggler() {
+  useEffect(() => {
+    retrieveAndApplyTheme();
+  });
+  let [currentTheme, setCurrentTheme] = useState(localStorage.getItem('devfinder-theme') || 'light');
+  const nextTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  function retrieveAndApplyTheme() {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  }
+  function toggleTheme() {
+    setTheme(nextTheme);
+  }
+  function setTheme(theme) {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('devfinder-theme', theme);
+    setCurrentTheme(theme)
+  }
   return (
-    <label htmlFor="">
-      {theme}
+    <label className="theme-toggler" onClick={toggleTheme}>
+      <span className="switch-to">{nextTheme}</span>
+      { nextTheme === 'dark' ? <IconMoon /> : <IconSun /> }
     </label>
-  )
+  );
 }
 
 export default ThemeToggler;
