07072c5c00581ffa20093d87d894eaefe4c2b43c
Towkir Ahmed <towkir17@gmail.com>
2023-11-22 01:44:38 +0600

    emitted username to parent from searchbar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import IconSearch from 'components/VectorIcons/IconSearch';
 import './SearchBar.css';
 
-function SearchBar() {
+function SearchBar({handleInput}) {
+  let [userName, setUserName] = useState('');
+  function handleUserNameChange(e) {
+    setUserName(e.target.value);
+  }
   return (
     <div className="searchbar">
       <IconSearch/>
-      <input type="text" placeholder="Search GitHub username…"/>
-      <button>Search</button>
+      <input type="text" value={userName} onChange={handleUserNameChange} placeholder="Search GitHub username…"/>
+      <button onClick={() => handleInput(userName)} disabled={userName.length === 0}>Search</button>
     </div>
   )
 }
