f512312d0384a5fe5cb04f53c7e6749f6c8372ab
Towkir Ahmed <towkir17@gmail.com>
2023-11-22 01:45:40 +0600

    received username and fetched data from api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { useState } from 'react';
 import Header from 'components/Header/Header';
-import SearchBar from "components/SearchBar/SearchBar";
+import SearchBar from 'components/SearchBar/SearchBar';
 import './App.css';
 
 function App() {
+  let [userData, setUserData] = useState({});
+  let [error, setError] = useState({});
+  function fetchUserData(username) {
+    fetch(`https://api.github.com/users/${username}`).then(response => {
+      let status = response.ok;
+      response.json().then((data) => {
+        if (status) {
+          setUserData(data);
+        } else {
+          setError(data);
+        }
+      })
+    })
+  }
   return (
     <div className="app">
       <Header/>
-      <SearchBar/>
+      <SearchBar handleInput={fetchUserData}/>
     </div>
   );
 }
