17a453c31b26da7cf708181ed3848d87fc453c76
Towkir Ahmed <towkir17@gmail.com>
2023-11-28 23:39:46 +0600

    centered the app container

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import Header from 'components/Header/Header';
 import SearchBar from 'components/SearchBar/SearchBar';
 import UserCard from 'components/UserCard';
-import './App.css';
 
 function App() {
   let [userData, setUserData] = useState({});
