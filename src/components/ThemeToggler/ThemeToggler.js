4278a163a62a366e865f390746c4438cc654cbb2
Towkir Ahmed <towkir17@gmail.com>
2023-12-02 15:19:15 +0600

    fixed useEffect and refactors functions of theme toggler

--- src/components/ThemeToggler/ThemeToggler.js
+++ src/components/ThemeToggler/ThemeToggler.js
@@ -3,24 +3,20 @@ import IconMoon from 'components/VectorIcons/IconMoon';
 import IconSun from 'components/VectorIcons/IconSun';
 import './ThemeToggler.css';
 function ThemeToggler() {
-  useEffect(() => {
-    retrieveAndApplyTheme();
-  });
   let [currentTheme, setCurrentTheme] = useState(localStorage.getItem('devfinder-theme') || 'light');
   const nextTheme = currentTheme === 'dark' ? 'light' : 'dark';
-  function retrieveAndApplyTheme() {
+  function applyThemeToPage() {
     document.documentElement.setAttribute('data-theme', currentTheme);
   }
-  function toggleTheme() {
-    setTheme(nextTheme);
-  }
-  function setTheme(theme) {
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('devfinder-theme', theme);
+  function saveThemeSettings(theme) {
     setCurrentTheme(theme)
+    localStorage.setItem('devfinder-theme', theme);
   }
+  useEffect(() => {
+    applyThemeToPage();
+  }, [currentTheme]);
   return (
-    <label className="theme-toggler" onClick={toggleTheme}>
+    <label className="theme-toggler" onClick={() => saveThemeSettings(nextTheme)}>
       <span className="switch-to">{nextTheme}</span>
       { nextTheme === 'dark' ? <IconMoon /> : <IconSun /> }
     </label>
