cdded4b14dc965550de6a133e7a307836c9639b4
Towkir Ahmed <towkir17@gmail.com>
2023-12-13 00:46:13 +0600

    enabled form submission or enter press to search user

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,6 +9,10 @@ function SearchBar({handleInput, errorMessage}) {
     setUserName(e.target.value);
     setInputValueChanged(true);
   }
+  function submitForm(event) {
+    event.preventDefault();
+    handleInput(userName);
+  }
   useEffect(() => {
     handleInput(userName);
   }, []);
@@ -16,12 +20,12 @@ function SearchBar({handleInput, errorMessage}) {
     setInputValueChanged(false);
   }, [errorMessage]);
   return (
-    <div className="searchbar">
+    <form className="searchbar" onSubmit={submitForm}>
       <IconSearch/>
       <input type="text" value={userName} onChange={handleUserNameChange} placeholder="Search GitHub username…"/>
       <span className="error">{!inputValueChanged ? errorMessage : ''}</span>
-      <button onClick={() => handleInput(userName)} disabled={userName.length === 0}>Search</button>
-    </div>
+      <button type="submit" disabled={userName.length === 0}>Search</button>
+    </form>
   )
 }
 
